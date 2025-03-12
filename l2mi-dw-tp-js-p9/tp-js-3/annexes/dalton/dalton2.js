8acc1563c7961553159878017f2c745e0ceb32ae
mamadouwhile <mahamadou.dembele@etud.univ-angers.fr>
2025-03-12 16:10:22 +0100

    Mise à jour du fichier dalton2.js

--- l2mi-dw-tp-js-p9/tp-js-3/annexes/dalton/dalton2.js
+++ l2mi-dw-tp-js-p9/tp-js-3/annexes/dalton/dalton2.js
@@ -1,8 +1,8 @@
 
-Dalton.prototype.nom = "Dalton";
+Dalton.prototype.nom = "Dalton"
 
 Dalton.prototype.afficher = function() {
-    return this.nom + " " + this.prenom;
+    console.log(this.nom + " " + this.prenom);
 };
 
 const daltons = [averell, jack, joe, william];
@@ -13,29 +13,23 @@ for (let x of daltons) {
 
 daltons.map(dalton => dalton.afficher());
 
-console.log(william.hasOwnProperty("nom")); 
+console.log(william.hasOwnProperty("nom"));
 
-for (let key in william) {
-    console.log(key);
+for(let key in william){
+    console.log(key)
 }
+console.log("Avant suppression");
 
-for (let dalton of daltons) {
-    for (let key in dalton) {
-        console.log(key);
-    }
-}
-
-
-for (let dalton of daltons) {
-    dalton.afficher(); 
+for(let dalton of daltons){
+    for(let key in dalton){
+        console.log(key)
+    }   
 }
 
 delete Dalton.prototype.nom;
-
-
-
-
-
-for (let dalton of daltons) {
-    dalton.afficher(); 
+console.log("Après suppression");
+for(let dalton of daltons){
+    for(let key in dalton){
+        console.log(key)
+    }   
 }
