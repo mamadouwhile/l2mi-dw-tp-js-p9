d03ce7bfe8832552bd53e6c12f40c1ec694546d3
mamadouwhile <mahamadou.dembele@etud.univ-angers.fr>
2025-03-17 13:23:24 +0100

    Correction de l'exercice 4, partie 2

---
+++ l2mi-dw-tp-js-p9/tp-js-4/annexes/dedom/dedom.js
@@ -0,0 +1,29 @@
+//Question1
+let form = document.querySelector("form");
+form.previousElementSibling.remove();
+form.remove();
+
+//Question2
+document.querySelectorAll("table tr").forEach(e => {
+    e.style.textAlign = "center";
+});
+
+//Question3
+document.querySelectorAll("table td").forEach(e =>{
+    e.style.border = "1px solid violet";
+});
+
+//Question4
+let paragraphe = document.getElementById("intro");
+paragraphe.nextElementSibling.style.display ="none";
+paragraphe.style.display ="none";
+
+//Question5
+let liste1 = document.querySelector("ol");
+liste1.querySelectorAll("li").forEach(li => {
+    if (li.textContent.startsWith("D")) {
+        li.style.fontFamily = "Cursive";
+    } else {
+        li.textContent = "⚠️";
+    }
+});
